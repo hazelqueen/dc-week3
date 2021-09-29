8d65bd454605700ee7b424fbb6375d37f59c87f2
judyann03 <judyanndayo03@gmail.com>
2021-09-29 15:44:07 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,15 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "Hazel Queen Swift", age:22 + " years old", gender: "Female"},
+            {name: "Taylor Swift", age:32 + " years old", gender: "Female"},
+            {name: "Joe Alwyn", age:30 + " years old", gender: "Male"},
+            {name: "Cardi B", age:35 + " years old", gender: "Female"},
+            {name: "Gordon Ramsay", age:54 + " years old", gender: "Male"},
+            {name: "Schrondingers Cat", age:18 + " years old", gender: "Male"},
+            {name: "Jane Austin", age:80 + " years old", gender: "Female"},
+            {name: "June July", age:24 + " years old", gender: "Male"},
+        ];
+    }
+
+}
